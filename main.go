b2ec49d3f887bd3097371f14548f5d8a55338924
Wenhui Shen <swhbox@foxmail.com>
2016-08-30 02:13:02 +0800

    update

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
 	"path/filepath"
+	"regexp"
+	"strings"
 
+	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/middleware/markdown"
 	X "github.com/webx-top/webx"
 	"github.com/webx-top/webx/lib/com"
+)
+
+var (
+	includeTag = regexp.MustCompile(`\{\%\s*include\s*"[^"]+"\s*\%\}`)
+	fileQuotes = regexp.MustCompile(`"([^"]+)"`)
 )
 
 func main() {
@@ -24,6 +33,33 @@ func main() {
 			}
 			return true
 		},
+	}
+	markdownOptions.Preprocessor = func(c echo.Context, b []byte) []byte {
+		if strings.HasPrefix(c.Request().URL().Path(), `/gopl-zh/`) {
+			s := string(b)
+			s = includeTag.ReplaceAllStringFunc(s, func(v string) string {
+				vs := fileQuotes.FindAllString(v, 1)
+				if len(vs) > 0 {
+					vs[0] = strings.TrimPrefix(vs[0], `"`)
+					vs[0] = strings.TrimSuffix(vs[0], `"`)
+					fpath := filepath.Join(markdownOptions.Root, c.Request().URL().Path())
+					fpath = filepath.Dir(fpath)
+					for strings.Contains(vs[0], `../`) {
+						fpath = filepath.Dir(fpath)
+						vs[0] = strings.Replace(vs[0], `../`, ``, 1)
+					}
+					fpath = filepath.Join(fpath, vs[0])
+					bt, err := ioutil.ReadFile(fpath)
+					if err == nil {
+						return string(bt)
+					}
+					println(err.Error())
+				}
+				return v
+			})
+			return []byte(s)
+		}
+		return b
 	}
 	server := X.Serv()
 	server.Use(markdown.Markdown(markdownOptions))
