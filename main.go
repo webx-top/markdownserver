49a57c5ab38660a38d860faa85d238b3ec611a23
swh <swhbox@foxmail.com>
2016-08-30 15:22:51 +0800

    update

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/admpub/confl"
+	"github.com/admpub/log"
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/middleware/markdown"
 	X "github.com/webx-top/webx"
@@ -19,12 +21,71 @@ var (
 	fileQuotes = regexp.MustCompile(`"([^"]+)"`)
 	fileRel    = regexp.MustCompile(`\([^\)]+\)`)
 	imgTag     = regexp.MustCompile(`\!\[[^\]]*\]\(\.\./[^\)]+\)`)
+	Parses     = map[string]*ParseSetting{}
 )
 
+type ParseSetting struct {
+	Include bool `json:"include"`
+	Image   bool `json:"image"`
+}
+
 func main() {
-	port := flag.String(`p`, `8080`, ``)
+	port := flag.String(`p`, `8080`, `-p 8080`)
+	parse := flag.String(`parse`, `/gopl-zh/:include,image;`, `-parse "路径前缀:解析项目清单"`)
+	configFile := flag.String(`c`, `data/config/config.yml`, `-c data/config/config.yml`)
 	flag.Parse()
 
+	if len(*parse) > 0 {
+		*parse = strings.TrimRight(*parse, `;`)
+		for _, item := range strings.Split(*parse, `;`) {
+			vs := strings.SplitN(item, `:`, 2)
+			if len(vs) > 1 {
+				c := &ParseSetting{}
+				for _, v := range strings.Split(vs[1], `,`) {
+					if v == `include` {
+						c.Include = true
+						continue
+					}
+					if v == `image` {
+						c.Image = true
+						continue
+					}
+				}
+				Parses[vs[0]] = c
+			}
+		}
+	}
+
+	if len(*configFile) > 0 {
+		if com.FileExists(*configFile) {
+			Parses = map[string]*ParseSetting{}
+			_, err := confl.DecodeFile(*configFile, &Parses)
+			if err != nil {
+				log.Error(err)
+			}
+		}
+		actions := com.MonitorEventFunc{
+			Modify: func(name string) {
+				log.Info(`Reload ` + *configFile)
+				Parses = map[string]*ParseSetting{}
+				_, err := confl.DecodeFile(*configFile, &Parses)
+				if err != nil {
+					log.Error(err)
+				}
+				log.Warnf(com.Dump(Parses, true))
+			},
+		}
+		actions.Create = actions.Modify
+		configFileName := filepath.Base(*configFile)
+		go func() {
+			err := com.Monitor(filepath.Dir(*configFile), actions, func(name string) bool {
+				return strings.HasSuffix(name, configFileName)
+			})
+			if err != nil {
+				log.Error(err)
+			}
+		}()
+	}
 	markdownOptions := &markdown.Options{
 		Path:   `/`,
 		Root:   filepath.Join(com.SelfDir(), `data/markdown`),
@@ -38,48 +99,56 @@ func main() {
 		},
 	}
 	markdownOptions.Preprocessor = func(c echo.Context, b []byte) []byte {
-		if strings.HasPrefix(c.Request().URL().Path(), `/gopl-zh/`) {
+		urlPath := c.Request().URL().Path()
+		for prefixPath, config := range Parses {
+			if !strings.HasPrefix(urlPath, prefixPath) {
+				continue
+			}
 			s := string(b)
-			ppath := filepath.Join(markdownOptions.Root, c.Request().URL().Path())
-			ppath = filepath.Dir(ppath)
-			s = includeTag.ReplaceAllStringFunc(s, func(v string) string {
-				vs := fileQuotes.FindAllString(v, 1)
-				if len(vs) > 0 {
-					vs[0] = strings.TrimPrefix(vs[0], `"`)
-					vs[0] = strings.TrimSuffix(vs[0], `"`)
-					fpath := ppath
-					for strings.Contains(vs[0], `../`) {
-						fpath = filepath.Dir(fpath)
-						vs[0] = strings.Replace(vs[0], `../`, ``, 1)
+			absPath := filepath.Join(markdownOptions.Root, urlPath)
+			absPath = filepath.Dir(absPath)
+			ppath := path.Dir(urlPath)
+			if config.Include {
+				//解析 {% include "file.md" %}
+				s = includeTag.ReplaceAllStringFunc(s, func(v string) string {
+					vs := fileQuotes.FindAllString(v, 1)
+					if len(vs) > 0 {
+						vs[0] = strings.TrimPrefix(vs[0], `"`)
+						vs[0] = strings.TrimSuffix(vs[0], `"`)
+						fpath := absPath
+						for strings.Contains(vs[0], `../`) {
+							fpath = filepath.Dir(fpath)
+							vs[0] = strings.Replace(vs[0], `../`, ``, 1)
+						}
+						fpath = filepath.Join(fpath, vs[0])
+						bt, err := ioutil.ReadFile(fpath)
+						if err == nil {
+							return string(bt)
+						}
+						println(err.Error())
 					}
-					fpath = filepath.Join(fpath, vs[0])
-					bt, err := ioutil.ReadFile(fpath)
-					if err == nil {
-						return string(bt)
+					return v
+				})
+			}
+			if config.Image {
+				//修正markdown图片网址
+				s = imgTag.ReplaceAllStringFunc(s, func(v string) string {
+					vs := fileRel.FindAllString(v, 1)
+					if len(vs) > 0 {
+						orig := vs[0]
+						vs[0] = strings.TrimPrefix(vs[0], `(`)
+						vs[0] = strings.TrimSuffix(vs[0], `)`)
+						fpath := ppath
+						for strings.Contains(vs[0], `../`) {
+							fpath = path.Dir(fpath)
+							vs[0] = strings.Replace(vs[0], `../`, ``, 1)
+						}
+						fpath = path.Join(fpath, vs[0])
+						v = strings.Replace(v, orig, `(`+fpath+`)`, 1)
 					}
-					println(err.Error())
-				}
-				return v
-			})
-
-			ppath = c.Request().URL().Path()
-			ppath = path.Dir(ppath)
-			s = imgTag.ReplaceAllStringFunc(s, func(v string) string {
-				vs := fileRel.FindAllString(v, 1)
-				if len(vs) > 0 {
-					orig := vs[0]
-					vs[0] = strings.TrimPrefix(vs[0], `(`)
-					vs[0] = strings.TrimSuffix(vs[0], `)`)
-					fpath := ppath
-					for strings.Contains(vs[0], `../`) {
-						fpath = path.Dir(fpath)
-						vs[0] = strings.Replace(vs[0], `../`, ``, 1)
-					}
-					fpath = path.Join(fpath, vs[0])
-					v = strings.Replace(v, orig, `(`+fpath+`)`, 1)
-				}
-				return v
-			})
+					return v
+				})
+			}
 			return []byte(s)
 		}
 		return b
